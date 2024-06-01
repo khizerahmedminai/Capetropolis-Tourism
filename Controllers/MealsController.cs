c1c2b84599d27338203f4c53462bd30bec5d6df6
Ahmed Raza <kingminai3@gmail.com>
2024-06-01 15:37:47 +0200

    added the base html files

---
+++ Controllers/MealsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CapetropolisTourism.Models;
+
+namespace CapetropolisTourism.Controllers;
+
+public class MealsController : Controller
+{
+    private readonly ILogger<MealsController> _logger;
+
+    public MealsController(ILogger<MealsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
